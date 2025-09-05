11b00e3506815bd1b2aaefefb4fa18e8d4138c38
Bhavnoorgoyal <bbhavnoor_be24@thapar.edu>
2025-09-05 14:54:10 +0530

    Update Q1.cpp

--- Assignment 4/Q1.cpp
+++ Assignment 4/Q1.cpp
@@ -1,110 +1,73 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
-#define SIZE 100
-
-class SimpleQueue {
-    int arr[SIZE];
-    int front, rear;
-
-public:
-    SimpleQueue() {
-        front = -1;
-        rear = -1;
-    }
-
-    bool isFull() {
-        return rear == SIZE - 1;
-    }
-
-    bool isEmpty() {
-        return front == -1 || front > rear;
-    }
-
-    void enqueue(int val) {
-        if (isFull()) {
-            cout << "Queue is Full!\n";
-            return;
-        }
-        if (isEmpty()) front = 0;
-        arr[++rear] = val;
-        cout << "Enqueued: " << val << endl;
-    }
-
-    void dequeue() {
-        if (isEmpty()) {
-            cout << "Queue is Empty!\n";
-            return;
-        }
-        cout << "Dequeued: " << arr[front++] << endl;
-    }
-
-    void peek() {
-        if (isEmpty()) {
-            cout << "Queue is Empty!\n";
-            return;
-        }
-        cout << "Front element: " << arr[front] << endl;
-    }
-
-    void display() {
-        if (isEmpty()) {
-            cout << "Queue is Empty!\n";
-            return;
-        }
-        cout << "Queue elements: ";
-        for (int i = front; i <= rear; i++) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-    }
-};
-
 int main() {
-    SimpleQueue q;
+    queue<int> q;
     int choice, val;
 
     do {
-        cout << "\n=== Simple Queue Menu ===\n";
+        cout << "\n=== Simple queue Menu ===\n";
         cout << "1. Enqueue\n";
         cout << "2. Dequeue\n";
-        cout << "3. Peek (Front Element)\n";
+        cout << "3. Peek (Front)\n";
         cout << "4. isEmpty\n";
-        cout << "5. isFull\n";
+        cout << "5. Size\n";
         cout << "6. Display\n";
         cout << "0. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
 
         switch (choice) {
-        case 1:
-            cout << "Enter value to enqueue: ";
-            cin >> val;
-            q.enqueue(val);
-            break;
-        case 2:
-            q.dequeue();
-            break;
-        case 3:
-            q.peek();
-            break;
-        case 4:
-            cout << (q.isEmpty() ? "Queue is empty\n" : "Queue is not empty\n");
-            break;
-        case 5:
-            cout << (q.isFull() ? "Queue is full\n" : "Queue is not full\n");
-            break;
-        case 6:
-            q.display();
-            break;
-        case 0:
-            cout << "Exiting program.\n";
-            break;
-        default:
-            cout << "Invalid choice. Try again.\n";
+            case 1:
+                cout << "Enter value to enqueue: ";
+                cin >> val;
+                q.push(val);
+                cout << val << " enqueued.\n";
+                break;
+            case 2:
+                if (!q.empty()) {
+                    cout << q.front() << " dequeued.\n";
+                    q.pop();
+                } else {
+                    cout << "queue is empty!\n";
+                }
+                break;
+            case 3:
+                if (!q.empty())
+                    cout << "Front: " << q.front() << endl;
+                else
+                    cout << "queue is empty!\n";
+                break;
+            case 4:
+                cout << (q.empty() ? "queue is empty\n" : "queue is not empty\n");
+                break;
+            case 5:
+                cout << "Size: " << q.size() << endl;
+                break;
+            case 6:
+                if (q.empty()) {
+                    cout << "queue is empty!\n";
+                } else {
+                    cout << "queue elements: ";
+                    queue<int> temp = q;
+                    while (!temp.empty()) {
+                        cout << temp.front() << " ";
+                        temp.pop();
+                    }
+                    cout << endl;
+                }
+                break;
+            case 0:
+                cout << "Exiting...\n";
+                break;
+            default:
+                cout << "Invalid choice.\n";
         }
 
     } while (choice != 0);
 
     return 0;
 }
+
+
