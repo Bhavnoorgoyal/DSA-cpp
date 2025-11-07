745bb8f38a8d6d913322df17c2d80d78f1fa87a5
Bhavnoorgoyal <bbhavnoor_be24@thapar.edu>
2025-11-07 14:11:46 +0530

    Implement circular linked list size calculation

---
+++ assignment 6/q3(b).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node* next;
+};
+int sizeCircular(Node* head) {
+    if (head == NULL) return 0;
+
+    int count = 0;
+    Node* temp = head;
+    do {
+        count++;
+        temp = temp->next;
+    } while (temp != head);
+    return count;
+}
+int main() {
+    Node* head = new Node{1, NULL};
+    Node* second = new Node{2, NULL};
+    Node* third = new Node{3, NULL};
+  head->next = second;
+  second->next = third;
+  third->next = head;
+  cout << "Size of Circular Linked List: " << sizeCircular(head) << endl;
+  return 0;
+}
