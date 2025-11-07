691c17e27f006a85c7f71e39d8f71a216e483401
Bhavnoorgoyal <bbhavnoor_be24@thapar.edu>
2025-11-07 14:18:56 +0530

    Create q2.cpp

---
+++ assignment 7/q2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+void display(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+void improvedSelectionSort(int arr[], int n) {
+    int start = 0, end = n - 1;
+
+    while (start < end) {
+        int minIndex = start;
+        int maxIndex = start;
+
+        for (int i = start; i <= end; i++) {
+            if (arr[i] < arr[minIndex])
+                minIndex = i;
+            if (arr[i] > arr[maxIndex])
+                maxIndex = i;
+        }
+
+        swap(arr[start], arr[minIndex]);
+
+        if (maxIndex == start)
+            maxIndex = minIndex;
+
+        swap(arr[end], arr[maxIndex]);
+
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[50];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "\noriginal Array: ";
+    display(arr, n);
+
+    improvedSelectionSort(arr, n);
+
+    cout << "sorted Array: ";
+    display(arr, n);
+
+    return 0;
+}
